98637f6b7aaa171f95957400543fd3d2c2d09898
Extor3D <extor3d@gmail.com>
2023-01-31 17:43:11 -0300

    Codigo basico de vida del enemigo y un ejemplo de disparo

--- godot-project/src/characters/Enemies/Ant.gd
+++ godot-project/src/characters/Enemies/Ant.gd
@@ -4,6 +4,8 @@ extends Character
 
 @export var target: Node3D
 
+@export var health: int = 5
+
 func _target_position() -> Vector3:
 	return target.position
 
@@ -14,8 +16,19 @@ func _physics_process(delta):
 	else:
 		$AccelerationBehaviour.clear_target()
 
-
 func look_at_quat(quat):
 	var scale = global_transform.basis.get_scale()
 	global_transform.basis = Basis(quat).scaled(scale)
 
+func _on_hurt_area_body_entered(body):
+	if body.get_collision_layer() == Global.player_bullet_layer:
+		get_damaged(body.get_power())
+		
+func get_damaged(power):
+	health -= power
+	if health <= 0:
+		die()
+		
+func die():
+	queue_free()
+	
